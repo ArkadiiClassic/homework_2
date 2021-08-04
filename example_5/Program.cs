bd58b8e70b20c698248a4108cd568850fff06761
arkadii <arkadii.classsic@gmail.com>
2021-08-04 22:32:21 +0300

    change example_5

--- example_5/Program.cs
+++ example_5/Program.cs
@@ -12,6 +12,30 @@ namespace example_5
              * нужно ли человеку похудеть, набрать вес или всё в норме.
              *    б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.*/
 
+            Console.Write("Введите свой вес (кг): ");
+            var weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите свой рост (м): ");
+            var growth = Convert.ToDouble(Console.ReadLine());
+
+            var bmi = weight / (growth * growth);
+            Console.WriteLine($"Индекс массы тела: {bmi:F2};");
+
+            if (bmi < 18.5000)
+            {
+                Console.WriteLine("Недостаточный вес");
+            }
+            else if(18.50000 < bmi && bmi < 24.9999)
+            {
+                Console.WriteLine("Нормальный вес");
+            }
+            else if(25.0000 < bmi && bmi < 29.9999)
+            {
+                Console.WriteLine("Избыточный вес");
+            }
+            else
+            {
+                Console.WriteLine("Тучность");
+            }
         }
     }
 }

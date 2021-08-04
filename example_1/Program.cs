933ed8dadeacb1c6cbf040fb4072d7e0a3c25cf4
arkadii <arkadii.classsic@gmail.com>
2021-08-04 09:58:39 +0300

    Add project files.

---
+++ example_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Домашняя работа №2 пример_1
+            // Архиреев Аркадий 
+            /* 1. Написать метод, возвращающий минимальное из трёх чисел.*/
+
+            
+        }
+    }
+}
